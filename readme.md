c33e30e328080ca642f7b903267b36b46714643e
kibutan <s1170230@gmail.com>
2022-03-29 23:39:15 +0900

    create wordrow

--- readme.md
+++ readme.md
@@ -2,4 +2,4 @@
 
 https://www.youtube.com/watch?v=t_omcJmOQ_k&ab_channel=HarryWolff
 
-7:25
+27:37
--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { useState } from "react";
-import logo from "./logo.svg";
-import "./App.css";
-
-function App() {
+import WordRow from "./WordRow";
+export default function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="mx-auto w-96">
-      <h1 className="text-4xl text-center">Reacdle</h1>
+      <header className="border-b border-gray-500 pb-4 mb-2">
+        <h1 className="text-4xl text-center">Reacdle</h1>
+      </header>
+
+      <main>
+        <WordRow letters="hel" />
+        <WordRow letters="hell" />
+        <WordRow letters="hello" />
+      </main>
     </div>
   );
 }
-
-export default App;
