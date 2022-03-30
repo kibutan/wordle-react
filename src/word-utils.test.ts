82e1a6cbf7396f0a4b9699d58a39d5a479bd0b4e
kibutan <s1170230@gmail.com>
2022-03-30 22:23:41 +0900

    add all edge cases of our core algorithm

--- src/word-utils.test.ts
+++ src/word-utils.test.ts
@@ -39,12 +39,36 @@ describe("computeGuess", () => {
     ]);
   });
 
-  it("onlu does one match when two letters are present", () => {
+  it("only does one match when two letters are present", () => {
     expect(computeGuess("solid", "boost")).toEqual([
       LetterState.Present,
       LetterState.Match,
       LetterState.Miss,
       LetterState.Miss,
+      LetterState.Miss,
+    ]);
+  });
+  // //////////////////////////////////Copied///////////////////////////////////////
+  test("returns empty array when given incomplete guess", () => {
+    expect(computeGuess("so", "boost")).toEqual([]);
+  });
+
+  test("when 2 letters are present but answer has only 1 of those letters", () => {
+    expect(computeGuess("allol", "smelt")).toEqual([
+      LetterState.Miss,
+      LetterState.Present,
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Miss,
+    ]);
+  });
+
+  test("when 1 letter matches but guess has more of the same letter", () => {
+    expect(computeGuess("allol", "colon")).toEqual([
+      LetterState.Miss,
+      LetterState.Miss,
+      LetterState.Match,
+      LetterState.Match,
       LetterState.Miss,
     ]);
   });
--- src/word-utils.ts
+++ src/word-utils.ts
@@ -16,17 +16,51 @@ export function computeGuess(
   answerString: string
 ): LetterState[] {
   const result: LetterState[] = [];
-  const guessArray = guess.split("");
-  const answerArray = answerString.split("");
+  if (guess.length !== answerString.length) {
+    return result;
+  }
+  const answer = answerString.split("");
 
-  guessArray.forEach((letter, index) => {
-    if (letter === answerArray[index]) {
+  const guessAsArray = guess.split("");
+  const answerLetterCount: Record<string, number> = {};
+
+  guessAsArray.forEach((letter, index) => {
+    const currentAnswerLetter = answer[index];
+    answerLetterCount[currentAnswerLetter] = answerLetterCount[
+      currentAnswerLetter
+    ]
+      ? answerLetterCount[currentAnswerLetter] + 1
+      : 1;
+
+    if (currentAnswerLetter === letter) {
       result.push(LetterState.Match);
-    } else if (answerArray.includes(letter)) {
+    } else if (answer.includes(letter)) {
       result.push(LetterState.Present);
     } else {
       result.push(LetterState.Miss);
     }
   });
+
+  result.forEach((curResult, resultIndex) => {
+    if (curResult !== LetterState.Present) {
+      return;
+    }
+    const guessLetter = guessAsArray[resultIndex];
+
+    answer.forEach((currentAnswerLetter, answerIndex) => {
+      if (currentAnswerLetter !== guessLetter) {
+        return;
+      }
+      if (result[answerIndex] === LetterState.Match) {
+        result[resultIndex] = LetterState.Miss;
+      }
+      if (answerLetterCount[guessLetter] <= 0) {
+        result[resultIndex] = LetterState.Miss;
+      }
+      answerLetterCount[guessLetter]--;
+    });
+    return result;
+  });
+
   return result;
 }
