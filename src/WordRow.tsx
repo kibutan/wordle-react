acf2f6240118c0e1838e82389a4db3c90acf9212
kibutan <s1170230@gmail.com>
2022-04-02 23:59:54 +0900

    Make sure empty state renders correctly

--- src/WordRow.tsx
+++ src/WordRow.tsx
@@ -33,7 +33,9 @@ function CharacterBox({ value, state }: CharacterBoxProps) {
 
   return (
     <span
-      className={`inline-block border-2 border-gray-500 p-4 uppercase font-bold text-2xl text-center ${stateStyles}`}
+      className={`inline-block border-2 border-gray-500 p-4
+      before:inline-flex before:content-['_']
+      uppercase font-bold text-2xl text-center ${stateStyles}`}
       border-gray-500
     >
       {value}
